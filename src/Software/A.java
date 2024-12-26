2d28253c5771ba9dfa82830f2c18dd35721796dd
minja <minja@MJ>
2024-12-26 17:50:18 +0900

    Update A.java and B.java in devB

--- src/Software/A.java
+++ src/Software/A.java
@@ -3,6 +3,7 @@ package Software;
 public class A {
     public static void main(String[] args) {
         System.out.println("This is class A");
+        System.out.println("This is class A in dev B");
     }
 }
 
--- src/Software/B.java
+++ src/Software/B.java
@@ -3,6 +3,7 @@ package Software;
 public class B {
     public static void main(String[] args) {
         System.out.println("This is class B");
+        System.out.println("This is class B in dev B");
     }
 }
 
