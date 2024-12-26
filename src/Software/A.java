6126c6110b161c1a23cdc3830130f8c3e4ede025
minja <minja@MJ>
2024-12-26 17:51:07 +0900

    Update A.java and C.java in devC

--- src/Software/A.java
+++ src/Software/A.java
@@ -3,6 +3,8 @@ package Software;
 public class A {
     public static void main(String[] args) {
         System.out.println("This is class A");
+        System.out.println("This is class A in devC");
+        
     }
 }
 
--- src/Software/C.java
+++ src/Software/C.java
@@ -3,6 +3,7 @@ package Software;
 public class C {
     public static void main(String[] args) {
         System.out.println("This is class C");
+        System.out.println("This is class C in devC");
     }
 }
 
